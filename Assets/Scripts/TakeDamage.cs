02a977465c25acb803b22bc695b62263a42e28e0
Xwilarg <xwilarg@protonmail.com>
2022-12-24 18:45:31 +0100

    Add desk

--- Assets/Scripts/TakeDamage.cs
+++ Assets/Scripts/TakeDamage.cs
@@ -1,12 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class TakeDamage : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private TMP_Text _complain;
+
+    private string[] _complainList = new[]
+    {
+        "Stop that!",
+        "Outch!",
+        "I will call security!"
+    };
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _complain = GetComponentInChildren<TMP_Text>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -14,6 +24,7 @@ public class TakeDamage : MonoBehaviour
         if (collision.collider.CompareTag("Player"))
         {
             _rb.AddForce((transform.position - collision.collider.transform.position).normalized * 10f, ForceMode2D.Impulse);
+            _complain.text = _complainList[Random.Range(0, _complainList.Length)];
         }
     }
 }
